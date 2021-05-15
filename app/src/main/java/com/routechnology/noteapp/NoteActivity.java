7494fe7f62fc478014e93638e2c278b570b9ee67
SOG-web <raheemolalekanusman84@gmail.com>
2021-05-15 11:40:00 +0100

    test complete

--- app/src/main/java/com/routechnology/noteapp/NoteActivity.java
+++ app/src/main/java/com/routechnology/noteapp/NoteActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -21,6 +22,7 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
     public static final String NOTE_POSITION = "com.routechnology.noteapp.NOTE_POSITION";
     public static final int POSITION_NOT_SET = -1;
     private NoteInfo mNote;
@@ -46,6 +48,7 @@ public class NoteActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         if(mIsCancelling) {
+            Log.i(TAG, "Cancelling note at position" + mNotePosition);
             if(mIsNewNote) {
                 DataManager.getInstance().removeNote(mNotePosition);
             } else {
@@ -54,6 +57,10 @@ public class NoteActivity extends AppCompatActivity {
         } else {
             saveNote();
         }
+
+
+        Log.d(TAG, "onPause");
+
     }
 
     private void storePreviousNoteValues() {
@@ -103,6 +110,10 @@ public class NoteActivity extends AppCompatActivity {
 
         if(!mIsNewNote)
             displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteBody);
+
+
+        Log.d(TAG, "onCreate");
+
     }
 
     private void saveOriginalNoteValues() {
